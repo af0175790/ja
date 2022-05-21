245da9658f4469eba0c53f7855e5c46af4febe49
ayshwaryaa <102944251+af0175790@users.noreply.github.com>
2022-05-21 14:28:08 +0530

    Add files via upload

---
+++ lab activity/SpecifiedIndex.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList ;
+public class SpecifiedIndex {
+ 
+    public static void main(String[] args)
+    {
+ 
+        
+        ArrayList<Integer> al = new ArrayList<Integer>();
+ 
+        
+        al.add(3);
+        al.add(1);
+        al.add(7);
+        al.add(20);
+        al.add(5);
+        int val = 0;
+ while (al.size() > val) 
+ {
+ 
+    System.out.println(al.get(val));
+	val++ ;
+        }
+    }
+}
