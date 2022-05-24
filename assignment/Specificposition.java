e2153b429ee95e5dccb451eb6ab15effb66ddf52
ayshwaryaa <102944251+af0175790@users.noreply.github.com>
2022-05-24 13:54:16 +0530

    Add files via upload

---
+++ assignment/Specificposition.java
@@ -0,0 +1,11 @@
+public class Specificposition
+{
+
+public static void main(Strig args[])
+{
+
+String str = "Anudip foundation-best placement center";
+String newstr = str.substring(12,23);
+System.out.println("old String ="+str);
+System.out.println("new String ="+newstr);
+}}
