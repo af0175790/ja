d030e0ddc330a0ac9704ba1098a3f07b53852af6
ayshwaryaa <102944251+af0175790@users.noreply.github.com>
2022-05-24 13:55:21 +0530

    Add files via upload

---
+++ assignment/SecondMostFreq.java
@@ -0,0 +1,26 @@
+public class SecondMostFreq {
+    public static void main(String[] args) {
+    Solution solution = new Solution();
+    String a = "ababababd";
+    solution.solution(a);
+ }
+
+
+class Solution {
+     public void solution(String a) {
+         int[] array = new int[26];
+         char[] char_array = a.toCharArray();
+
+         for(int i = 0; i < char_array.length; i++) {
+             int index = char_array[i] - 'a';
+             array[index]++;
+         }
+         int max = 0;
+         for(int i = 0; i < array.length; i++) {
+             if(array[i] > max) {
+                 max = array[i];
+             }
+         }
+     }
+ }
+}
