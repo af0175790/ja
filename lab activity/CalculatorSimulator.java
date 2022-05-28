46af02b00ce88a1a41786d864270f2b1afb6ae01
ayshwaryaa <102944251+af0175790@users.noreply.github.com>
2022-05-28 09:25:13 +0530

    Add files via upload

---
+++ lab activity/CalculatorSimulator.java
@@ -0,0 +1,96 @@
+
+class TaxCalculator {  
+    
+String empName; 
+boolean isIndian;
+double empSal  ;
+  
+ 
+public String getempName() {  
+    return empName;  
+}  
+public void getempName(String empName) {  
+    this.empName = empName;  
+}  
+public int getisIndian() {  
+    return isIndian;  
+}  
+public void getisIndian(boolean isIndian) {  
+    this.empName = empName;  
+}  
+public double getempSal() {  
+    return empSal;  
+}  
+public void getempSal(double empSal) {  
+    this.empSal = empSal;  
+}  
+ 
+ 
+@Override  
+public  calculateTax () {  
+    return  TaxCalculator(empName = " + empName + ", isIndian= " + isIndian + ", empSal= " + empSal+ ");  
+    }  
+      
+	  System.out.println(empName);  
+        double empSal = emp.getSalary();  
+        double increment = 0;  
+        
+        if (empsal<=10000)  
+        {  
+            taxAmount=empSal*8/100; 
+            System.out.println(empSal);  
+              
+        }else if 
+		{((empsal 50000) && (empsal 10000 )){  
+             taxAmount=empSal*6/100; 
+            System.out.println(empSal);  
+        }else  if 
+		{ (( empSal 30000)&&(empSal 50000)) 
+              taxAmount=empSal*5/100; 
+            System.out.println(empSal);  
+        }else if
+		{((empsal 10000)&&(empSal 30000))
+		       taxAmount=empSal*4/100; 
+            System.out.println(empSal);   
+		} else {
+		System.out.println("TaxNotEligibleException");
+			
+
+ 
+public class CalculatorSimulator{  
+     
+    public static void main(String args[]) {  
+        
+        
+        TaxCalculator emp = new TaxCalculator();  
+        System.out.println("TsxAmount is"+<TaxAmount>);
+		try{
+		 
+			if(Country not valid)
+			{
+				System.out.println("The employee should be an Indin citizen for calculationtax");
+			}
+			else(Employee name not valid)
+			{
+				System.out.println("The employee name cannot be empty");
+			}
+			else(Not eligible for Tax calculation)
+			{
+			System.out.println("The employee does not need to paytax");
+			}
+		}catch(exception e)    
+		emp.setemployeeName("ron");  
+        emp.setEmployeeSalary(34000);  
+        emp.setisIndian(false);
+		emp.setemployeeName("tim");  
+        emp.setEmployeeSalary(1000);  
+        emp.setisIndian(True);  
+		emp.setemployeeName("jack");  
+        emp.setEmployeeSalary(55000);  
+        emp.setisIndian(true); 
+		emp.setemployeeName(" ");  
+        emp.setEmployeeSalary(30000);  
+        emp.setisIndian(true);  
+        
+    }  
+}  
